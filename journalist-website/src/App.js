34e871d5ca589ff23b7e46d2fa0053648ecd2344
Tw33t3r <garrettjtvedt@gmail.com>
2021-04-23 16:31:47 -0600

    initial setup

---
+++ journalist-website/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import './assets/bootstrap.css'
+
+import Navbar from './navigationMenu/navigationMenu'
+import Main from './main/Main'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ journalist-website/src/navigationMenu/navigationMenu.js
@@ -0,0 +1,18 @@
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+
+function NavigationMenu(){
+  return(
+    <Navbar bg="light" expand="lg" sticky="top">
+    <Navbar.Brand href="#home">Isaiah</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#link">Link</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+  );
+}
+export default NavigationMenu;
