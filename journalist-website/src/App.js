695e33f577af43b028eecca2ab97d83a7c4f2e4f
Tw33t3r <garrettjtvedt@gmail.com>
2021-04-23 19:08:37 -0600

    Navbar and carousel

--- journalist-website/src/App.js
+++ journalist-website/src/App.js
@@ -1,16 +1,39 @@
 import './App.css';
 import './assets/bootstrap.css'
 
-import Navbar from './navigationMenu/navigationMenu'
-import Main from './main/Main'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 
-function App() {
+import Home from './pages/Home'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+export default function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Main />
-    </div>
+    <Router>
+      <div>
+        <Navbar className="navbar-dark navbar-fixed-top" expand="lg" sticky="top">
+          <Navbar.Brand href="#home">Isaiah</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
--- journalist-website/src/pages/Home.js
+++ journalist-website/src/pages/Home.js
@@ -1,8 +1,53 @@
-function Home(){
-    return(
-        <div>
-            Hello World!
-        </div>
+import './Home.css';
+
+import Carousel from 'react-bootstrap/Carousel';
+
+import home1 from "../assets/thumbnail_image1.png";
+import home2 from "../assets/thumbnail_image2.png";
+import home3 from "../assets/cool.jpeg";
+
+function Home() {
+    return (
+        <Carousel fade>
+            <Carousel.Item>
+                <div className="overflow">
+                    <img
+                        className="d-block w-100"
+                        src={home1}
+                        alt="First slide"
+                    />
+                </div>
+                <div className="image1caption">
+                    <h1>Your Production Swiss Army Knife</h1>
+                </div>
+            </Carousel.Item>
+            <Carousel.Item>
+                <div className="overflow">
+                    <img
+                        className="d-block w-100"
+                        src={home2}
+                        alt="Second slide"
+                    />
+                </div>
+                <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <div className="overflow">
+                    <img
+                        className="d-block w-100"
+                        src={home3}
+                        alt="Third slide"
+                    />
+                </div>
+                <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
     );
 }
 
